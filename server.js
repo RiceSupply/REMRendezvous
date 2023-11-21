8264da23659b74e667d73baf5e78f18fe241198e
Alex Le <140272444+RiceSupply@users.noreply.github.com>
2023-11-21 15:56:04 -0800

    moved middleware

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const { User } = require('./models');
 const { SleepCycle } = require('./models');
 
-const hbs = exphbs.create({ helpers });
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
