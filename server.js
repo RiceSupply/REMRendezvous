c606d18a6d82865a972d03f1a1d80b5beb9fcf40
Alex Le <140272444+RiceSupply@users.noreply.github.com>
2023-11-21 15:53:25 -0800

    added more pathways

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const routes = require('./routes');
+const homeControl = require('./controllers/homeController');
+const sleepyCycleControl = require('./controllers/sleepyCycleController.js');
+const userControl = require('./controllers/userController');
+const helpers = require('./utils/helper');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
