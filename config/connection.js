53c5eb2ecdb4a21892858ccb68c08749f243ea96
Alex Le <140272444+RiceSupply@users.noreply.github.com>
2023-11-21 11:52:47 -0800

    Switched over to sequelize with SQL queries instead

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,16 @@
 require('dotenv').config();
-const mysql = require('mysql2');
 const Sequelize = require('sequelize');
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-});
-
-connection.promise();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: 'localhost',
+    host: 'localhost', //127.0.0.1
     dialect: 'mysql',
     port: 3306
   }
 );
 
-module.exports = { connection, sequelize };
+module.exports = sequelize;
+
